584b262a1ad9859098f3625cd18655f2007d4700
Wonho-mis <37608231+six6929@users.noreply.github.com>
2018-07-26 14:11:04 +0900

    Add files via upload

---
+++ 01_Hello/src/Ex01.java
@@ -0,0 +1,19 @@
+
+public class Ex01 {
+	public static void main(String[] args) {
+		//한 줄 주석문을 사용할 때 사용하는 부호
+		/* 
+		 여러 줄에 대하여 주석처리하는 부호
+		  */
+		//처음 작성한 자바프로그램
+		/*
+		 * 여러줄 주석
+		 * 주석 계속
+		 */
+		/** 
+		 * 메뉴얼 작성용 주석
+		 */
+		System.out.println("Hello World~~!!!!"); //헬로월드
+		System.out.println("Hello Java!!!");
+	}
+}
---
+++ 01_Hello/src/Ex02.java
@@ -0,0 +1,29 @@
+/* 
+ 화면(콘솔, console)상에 별(*) 5개를 붙여서 출력
+ *****
+ ****
+ ***
+ **
+ *
+ -----
+ *
+ **
+ ***
+ ****
+ *****
+ */
+public class Ex02 {
+	public static void main(String[] args) {
+		System.out.println("*****");
+		System.out.println("****");
+		System.out.println("***");
+		System.out.println("**");
+		System.out.println("*");
+		System.out.println("-----");
+		System.out.println("*");
+		System.out.println("**");
+		System.out.println("***");
+		System.out.println("****");
+		System.out.println("*****");
+	}
+}
