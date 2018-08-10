36f0b8d4fa391796c6b85c55639c5aca9cfb596d
Wonho-mis <37608231+six6929@users.noreply.github.com>
2018-08-10 18:03:23 +0900

    Add files via upload

---
+++ 13_Map_Stack_Queue/src/com/mystudy/pm03_scanner/ScannerExam.java
@@ -0,0 +1,29 @@
+package com.mystudy.pm03_scanner;
+
+import java.util.Scanner;
+
+public class ScannerExam {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		System.out.println("문자열 1 : ");
+		String str = sc.nextLine();
+		System.out.println(">>> " + str);
+		
+		
+		System.out.println("문자열 2 : ");
+		String str2 = sc.nextLine();
+		System.out.println(">>> " + str2);
+		
+		System.out.println("-----------------------");
+		System.out.println("정수값 2개를 입력하면 더한 값 출력");
+		System.out.println("숫자 1: ");
+		int num1 = sc.nextInt();
+		
+		System.out.println("숫자2 : ");
+		int num2 = sc.nextInt();
+		sc.nextLine();
+		System.out.println(num1 + "+" + num2 + " = " + (num1+num2));
+	}
+
+}
