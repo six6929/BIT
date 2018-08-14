6eeb45a95050e038f815182f2ad40ff03a92a5c0
Wonho-mis <37608231+six6929@users.noreply.github.com>
2018-08-14 18:05:51 +0900

    Add files via upload

---
+++ Homework0808/src/StudentTest.java
@@ -0,0 +1,79 @@
+import java.util.*;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		StudentVO stu1 = new StudentVO("광", 100, 80, 90);
+		StudentVO stu2 = new StudentVO("개", 95, 75, 95);
+		StudentVO stu3 = new StudentVO("토", 80, 90, 100);
+		StudentVO stu4 = new StudentVO("대", 50, 60, 70);
+		StudentVO stu5 = new StudentVO("왕", 100, 80, 100);
+	
+//		System.out.println(stu1.toString());
+//		ArrayList list1 = new ArrayList(5);
+//		ArrayList list2 = new ArrayList(5);
+//		ArrayList list3 = new ArrayList(5);
+//		ArrayList list4 = new ArrayList(5);
+//		ArrayList list5 = new ArrayList(5);
+//		
+//		System.out.println("------ 1번째 학생 성적------");
+//		list1.add(stu1);
+//		printData(list1);
+//		System.out.println();
+//		System.out.println("----1번째 학생 성적 정정 ----");
+//		stu1.setKor(80);
+//		printData(list1);
+//		
+//		System.out.println();
+//		System.out.println("------ 2번째 학생 성적 ------");
+//		list2.add(stu2);
+//		printData(list2);
+//		
+//		System.out.println();
+//		System.out.println("------ 3번째 학생 성적 ------");
+//		list3.add(stu3);
+//		printData(list3);
+//		
+//		System.out.println();
+//		System.out.println("------ 4번째 학생 성적 ------");
+//		list4.add(stu4);
+//		printData(list4);
+//		System.out.println();
+//		System.out.println("------ 4번째 학생 성적 정정 ------");
+//		stu4.setEng(85);
+//		stu4.setKor(76);
+//		stu4.setMath(93);
+//		printData(list4);
+//		
+//		System.out.println();
+//		System.out.println("------ 5번째 학생 성적 ------");
+//		list5.add(stu5);
+//		printData(list5);
+//		
+		ArrayList list = new ArrayList(5);
+
+		list.add(stu1);
+		list.add(stu2);
+		list.add(stu3);
+		list.add(stu4);
+		list.add(stu5);
+		
+		printData(list);
+		System.out.println("-------stu1 국어성적 정정 --------");
+		stu1.setKor(55);
+		printData(list);
+		
+		System.out.println("-------stu4 성적 정정 ---------");
+		stu4.setKor(75);
+		stu4.setEng(97);
+		stu4.setKor(63);
+		printData(list);
+		
+	}
+	
+	static void printData(ArrayList list) {
+		for(int i=0; i<list.size(); i++) {
+			System.out.println("[index :" + i + "] " + list.get(i));
+		}
+	}
+}
---
+++ Homework0808/src/StudentVO.java
@@ -0,0 +1,114 @@
+public class StudentVO {
+	//필드선언
+	private String name;
+	private int kor;
+	private int eng;
+	private int math;
+	private int tot;
+	private double avg;
+	
+	//생성자 선언
+	public StudentVO() {
+		
+	}
+
+	public StudentVO(String name, int kor, int eng, int math) {
+		super();
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		computeTotAvg();
+	}
+	
+		
+	public StudentVO(String name, int kor, int eng, int math, int tot, double avg) {
+		super();
+		this.name = name;
+		this.kor = kor;
+		this.eng = eng;
+		this.math = math;
+		this.tot = tot;
+		this.avg = avg;
+		computeTotAvg();
+	}
+
+	//getter, setter
+	
+	public String getName() {
+		return name;
+	}
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getKor() {
+		return kor;
+	}
+	
+
+	public void setKor(int kor) {
+		this.kor = kor;
+		computeTotAvg();
+	}
+	
+	public int getEng() {
+		return eng;
+	}
+	
+	public void setEng(int eng) {
+		this.eng = eng;
+		computeTotAvg();
+	}
+	
+	public int getMath() {
+		return math;
+	}
+	
+	public void setMath(int math) {
+		this.math = math;
+		computeTotAvg();
+	}
+	
+	public int getTot() {
+		return tot;
+	}
+	
+//	public void setTot(int tot) {
+//		this.tot = tot;
+//	}
+	
+	public double getAvg() {
+		return avg;
+	}
+	
+//	public void setAvg(double avg) {
+//		this.avg = avg;
+//	}
+	
+	//총점,평균 계산 메서드
+	
+	public void computeTotAvg() {
+		this.tot = kor + eng + math;
+		this.avg = (tot * 100) / 3 / 100.0;
+	}
+
+
+	
+	//toString()
+	
+//	@Override
+//	public String toString() {
+//		return "StudentVO [name=" + name + ", kor=" + kor + ", eng=" + eng + ", math=" + math + ", tot=" + tot
+//				+ ", avg=" + avg + "]";
+//	}
+	
+	@Override
+	public String toString() {
+		return "학생성적 [이름=" + name + ", 국어점수=" + kor + ", 영어점수=" + eng + ", 수학점수=" + math + ", 총점=" + tot 
+				+ ", 평균=" + avg + "]";
+	}
+	
+	
+}
