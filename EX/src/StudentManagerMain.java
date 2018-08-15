0557bf75582bfdc10b0c563441518c488047fc2f
Wonho-mis <37608231+six6929@users.noreply.github.com>
2018-08-15 23:37:00 +0900

    Add files via upload

---
+++ EX/src/StudentManagerMain.java
@@ -0,0 +1,11 @@
+public class StudentManagerMain {
+
+	public static void main(String[] args) {
+		StudentManager manager = new StudentManager();
+		//StudentManager 클래스의 메소드 이용 CRUD 테스트
+		
+		
+
+	}
+
+}
