621ca7f6e1b85a00309f5fcff8cb2b954d50841e
Wonho-mis <37608231+six6929@users.noreply.github.com>
2018-08-22 17:26:28 +0900

    Add files via upload

---
+++ EX/EX/src/com/mystudy/manager/StudentManagerMain.java
@@ -0,0 +1,22 @@
+package com.mystudy.manager;
+
+public class StudentManagerMain {
+
+	public static void main(String[] args) {
+		StudentManager manager = new StudentManager();
+		//StudentManager 클래스의 메소드 이용 CRUD 테스트
+		
+		StudentVO stu = new StudentVO("001", "홍길동", 100, 90, 80);
+		StudentVO stu1 = new StudentVO("002", "홍길순", 95, 90, 85);
+		
+		manager.insert(stu);
+		manager.insert(stu1);
+
+		System.out.println();
+		
+		
+		
+
+	}
+
+}
